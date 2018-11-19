607723888faff97212ded1917da2a1676f9a0e02
Magdalena Brzozowska <magdalena.brzozowska94@wp.pl>
2018-11-19 15:07:26 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import DisplayMyName from './DisplayMyName';
 import DisplayName from './DisplayName'
 
+const name = 'Patryk Sztajdel'
+
 const App = () => (
   <div>
     <h1>Hello World</h1>
     < DisplayMyName />
     < DisplayName
-      name="Patryk Sztajdel" />
+      name={name} />
   </div>
 )
 
